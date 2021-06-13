3c8fe569c13d75cf0c5324978563b33b4f84e387
Ben <ben.peltier7@gmail.com>
2021-06-13 18:19:26 +0200

    rdy for props

--- GMTK 21/Assets/Scripts/InverseProps.cs
+++ GMTK 21/Assets/Scripts/InverseProps.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class InverseProps : MonoBehaviour
 {
-    
+    public Material materialRed;
+
+    public GameObject[] props;
+    GameObject currentProps;
+
+    private void Start()
+    {
+        props = GameObject.FindGameObjectsWithTag("Props");
+        foreach (GameObject item in props)
+        {
+            currentProps = Instantiate(item, new Vector3(-item.transform.position.x, item.transform.position.y, item.transform.position.z), Quaternion.identity, gameObject.transform);
+            print(currentProps);
+            if (currentProps.GetComponent<MeshRenderer>())
+                currentProps.GetComponent<MeshRenderer>().material = materialRed;
+        }
+    }
 }
