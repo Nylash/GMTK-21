9ed9f7e13a2f91c9c01b01bbbb86d429e595e9f6
Ben <ben.peltier7@gmail.com>
2021-06-13 19:32:38 +0200

    Auto stash before merge of "main" and "origin/main"

---
+++ GMTK 21/Assets/Scripts/StartScreen.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreen : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+            SceneManager.LoadScene("Level");
+    }
+}
