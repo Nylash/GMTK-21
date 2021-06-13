e9b8398aedeee1e8d7ffa7def37a8209517b29bd
Ben <ben.peltier7@gmail.com>
2021-06-13 16:45:43 +0200

    maj

--- GMTK 21/Assets/Scripts/CamerasMover.cs
+++ GMTK 21/Assets/Scripts/CamerasMover.cs
@@ -12,6 +12,8 @@ public class CamerasMover : MonoBehaviour
     void Update()
     {
         transform.position = new Vector3(Mathf.SmoothDamp(transform.position.x, follow.position.x -70, ref currentVelocity, speedDamp), transform.position.y, transform.position.z);
+        if (transform.position.x < -520)
+            transform.position = new Vector3(-520, transform.position.y, transform.position.z);
 
         otherCam.transform.position = new Vector3(-transform.position.x, otherCam.transform.position.y, otherCam.transform.position.z);
     }
